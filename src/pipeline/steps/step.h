2f6258ccfc3166a28d7f8576592ca7644bf6b659
Al <albarrentine@gmail.com>
2024-07-29 03:09:16 -0400

    [pipeline/step] pipeline_step needs to be a struct not a pointer for simulating inheritance, fixing stringify issue in define for step name and adding an additional typed options to pass an extra argument for steps where it is needed

--- src/pipeline/steps/step.h
+++ src/pipeline/steps/step.h
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
 
 typedef struct pipeline_step {
     char *type;
@@ -28,35 +30,54 @@ typedef struct pipeline_step {
 #define CONCAT(a, b) CONCAT_(a, b)
 #define STEP_TYPE(name) CONCAT(STEP_NAME, _##name)
 
+#define STRINGIFY_(x) #x
+#define STRINGIFY(x) STRINGIFY_(x)
+#define STEP_NAME_STR STRINGIFY(STEP_NAME)
+
+#ifndef STEP_OPTIONS_TYPE
 typedef OUTPUT_TYPE (*STEP_TYPE(func))(INPUT_TYPE);
+#else
+typedef OUTPUT_TYPE (*STEP_TYPE(func))(INPUT_TYPE, STEP_OPTIONS_TYPE);
+#endif
 typedef bool (*STEP_TYPE(validate))(INPUT_TYPE);
 
 typedef struct {
-    pipeline_step_t *head;
+    pipeline_step_t base;
     INPUT_TYPE input;
     OUTPUT_TYPE output;
+    #ifdef STEP_OPTIONS_TYPE
+    STEP_OPTIONS_TYPE options;
+    #endif
     STEP_TYPE(func) func;
     STEP_TYPE(validate) validate;
 } STEP_TYPE(step_t);
 
 
-static bool STEP_TYPE(run)(step_t *base, void *data) {
+static bool STEP_TYPE(run)(pipeline_step_t *base, void *data) {
     if (base == NULL) return false;
-    if (base->type != STEP_NAME) return false;
-    if (base->validate != NULL && !base->validate(((STEP_TYPE(step_t) *)base)->input)) return false;
+    if (strcmp(base->type, STEP_NAME_STR) != 0) return false;
     STEP_TYPE(step_t) *step = (STEP_TYPE(step_t) *)base;
     INPUT_TYPE input = (INPUT_TYPE)data;
     step->input = input;
-    step->output = step->func(input);
+    if (step->validate != NULL && !step->validate(step->input)) return false;
+    #ifndef STEP_OPTIONS_TYPE
+    step->output = step->func(step->input);
+    #else
+    step->output = step->func(input, step->options);
+    #endif
     return true;
 }
 
-static step_t STEP_TYPE(head) = { .type = STEP_NAME, .run = STEP_TYPE(run) };
+static const pipeline_step_t STEP_TYPE(base) = { .type = STEP_NAME_STR, .run = STEP_TYPE(run) };
 
+#ifndef STEP_OPTIONS_TYPE
 STEP_TYPE(step_t) *STEP_TYPE(new)(STEP_TYPE(func) func, STEP_TYPE(validate) validate) {
+#else
+STEP_TYPE(step_t) *STEP_TYPE(new)(STEP_TYPE(func) func, STEP_TYPE(validate) validate, STEP_OPTIONS_TYPE options) {
+#endif
     STEP_TYPE(step_t) *step = malloc(sizeof(STEP_TYPE(step_t)));
     if (step == NULL) return NULL;
-    step->head = &STEP_TYPE(head);
+    step->base = STEP_TYPE(base);
     step->input = (INPUT_TYPE)0;
     step->output = (OUTPUT_TYPE)0;
     step->validate = validate;
@@ -72,3 +93,6 @@ void STEP_TYPE(destroy)(STEP_TYPE(step_t) *step) {
 #undef CONCAT_
 #undef CONCAT
 #undef STEP_TYPE
+#undef STRINGIFY_
+#undef STRINGIFY
+#undef STEP_NAME_STR
